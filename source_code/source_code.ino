1164fcfaa38102119f07c82debf64a8823bb03e0
Malek Ghozzi <malek.ghozzi@insat.ucar.tn>
2025-10-08 17:15:37 +0100

    sama7ni

--- source_code/source_code.ino
+++ source_code/source_code.ino
@@ -6,7 +6,7 @@ int avg0s[8],
     avg1s[8],
     thrsh[8];
 
-==========================Calibration==================================
+
 
 void calibration(){
   digitalWrite(2, 1);
@@ -59,7 +59,7 @@ int get(int i){
   int a = analogRead(capteurs[i]);
   return a >= thrsh[i];
 }
-==========================================================================
+
 
 
 void move_for(int speed) {
