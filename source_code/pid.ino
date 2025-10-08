472d2344eabe0cd704df157ce51349d3e05894b0
awss1236 <awsssayah@gmail.com>
2025-10-08 19:13:15 +0100

    adjusted pid baby

--- source_code/pid.ino
+++ source_code/pid.ino
@@ -1,17 +1,28 @@
-void pid(){
-  float sum = 0;
+void pid(float kp, float kd){
+  float err = 0;
   int valsum = 0;
 
   // left to right
   for(int i=0;i<8;i++){
     int x = i < 4? i - 4 : i - 3;
     int g = get(i);
-    sum += g*x;
+    err += g*x;
     valsum += g;
   }
-  sum /= valsum;
-  Serial.print("Err: ");
-  Serial.print(sum);
-  //Serial.print("\n");
-}
+  if(valsum != 0)
+    err /= valsum;
 
+  float derr = err - lasterror;
+  lasterror = err;
+  // interr += err; // Ki == 0 for now
+  
+  float corr = kp*err+kd*derr;
+
+  int basespeed = 180;
+
+  int spd1 = constrain(basespeed + corr, 0, 255);
+  int spd2 = constrain(basespeed - corr, 0, 255);
+
+  motorAForward(spd1);
+  motorBForward(spd2);
+}
