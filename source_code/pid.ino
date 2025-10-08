98054d91fea2130eb209f351eac35eba65a5dc99
awss1236 <awsssayah@gmail.com>
2025-10-08 18:26:01 +0100

    started pid

---
+++ source_code/pid.ino
@@ -0,0 +1,16 @@
+void pid(){
+  float sum = 0;
+  int valsum = 0;
+
+  // left to right
+  for(int i=0;i<8;i++){
+    int x = i < 4? i - 4 : i - 3;
+    int g = get(i);
+    sum += g*x;
+    valsum += g;
+  }
+  sum /= valsum;
+  Serial.print("Err: ");
+  Serial.print(sum);
+  Serial.print("\n");
+}
