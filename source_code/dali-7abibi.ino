46848dd08e7227f1c80fa52933618e692368f5a7
Malek Ghozzi <malek.ghozzi@insat.ucar.tn>
2025-10-10 14:07:01 +0100

    suuu

---
+++ source_code/dali-7abibi.ino
@@ -0,0 +1,61 @@
+// // Motor A: IN1=19, IN2=21
+// // Motor B: IN3=23, IN4=22
+
+// // assign PWM channels
+// #define CH_A1 0
+// #define CH_A2 1
+// #define CH_B1 2
+// #define CH_B2 3
+
+// // ================= Motor setup =================
+// void setupMotors() {
+//   // Motor A
+//   ledcSetup(CH_A1, 5000, 8);  
+//   ledcAttachPin(19, CH_A1);   // IN1
+//   ledcSetup(CH_A2, 5000, 8);  
+//   ledcAttachPin(21, CH_A2);   // IN2
+
+//   // Motor B
+//   ledcSetup(CH_B1, 5000, 8);  
+//   ledcAttachPin(23, CH_B1);   // IN3
+//   ledcSetup(CH_B2, 5000, 8);  
+//   ledcAttachPin(22, CH_B2);   // IN4
+// }
+
+// // ================= Motor A =================
+// void motorAForward(int speed) {
+//   ledcWrite(CH_A1, constrain(speed, 0, 255));
+//   ledcWrite(CH_A2, 0);
+// }
+
+// void motorABackward(int speed) {
+//   ledcWrite(CH_A1, 0);
+//   ledcWrite(CH_A2, constrain(speed, 0, 255));
+// }
+
+// void motorAStop() {
+//   ledcWrite(CH_A1, 0);
+//   ledcWrite(CH_A2, 0);
+// }
+
+// // ================= Motor B =================
+// void motorBForward(int speed) {
+//   ledcWrite(CH_B1, constrain(speed, 0, 255));
+//   ledcWrite(CH_B2, 0);
+// }
+
+// void motorBBackward(int speed) {
+//   ledcWrite(CH_B1, 0);
+//   ledcWrite(CH_B2, constrain(speed, 0, 255));
+// }
+
+// void motorBStop() {
+//   ledcWrite(CH_B1, 0);
+//   ledcWrite(CH_B2, 0);
+// }
+
+// // ================= Both =================
+// void stopBoth() {
+//   motorAStop();
+//   motorBStop();
+// }
